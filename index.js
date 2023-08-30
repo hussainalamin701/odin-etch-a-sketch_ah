379c6d36353cf58a3c547e30a8ccdc944a46042c
hussain.a121145 <140621473+hussainalamin701@users.noreply.github.com>
2023-08-30 08:49:54 +0100

    Created my grid and added a hover effect

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const canvas = document.querySelector('#canvas');
+const elem = document.querySelectorAll('.element');
 
-let canvasSize = 32;
-//Create a function that will apply everything onto the canvas
+function changeSize(size){
+
+}
 
 function createGrid(size){
-    //Use for loops to go through 8 times
-    for(let i = 0; i < size; i++){
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+    for(let i = 0; i < numDivs; i++){
         const canvasElem = document.createElement('div');
-        canvasElem.style.width = '100px';
-        canvasElem.style.height = '100px';
-        canvasElem.style.backgroundColor = 'black';
+        canvasElem.classList.add('element');
+
+        canvas.insertAdjacentElement('beforeend', canvasElem);
     }
 }
 
-createGrid(8);
+let canvasSize = 16;
+createGrid(canvasSize);
